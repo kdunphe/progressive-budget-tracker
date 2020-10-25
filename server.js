25db945ca08167a63d826d3d26787144da847e26
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-10-25 17:36:40 -0400

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +17,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
